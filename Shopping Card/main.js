5753338d6a3d7407427a8086fef41f65450b193d
Purya <purya.B97@gmail.com>
2022-09-12 15:52:59 +0300

    Increment function ➕

--- Shopping Card/main.js
+++ Shopping Card/main.js
@@ -31,6 +31,8 @@ let shopItemsData = [
   },
 ];
 
+let basket = [];
+
 let generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
@@ -44,9 +46,9 @@ let generateShop = () => {
       <div class="price-quantitiy">
         <h2>${price}</h2>
         <div class="buttons">
-          <i class="bi bi-dash-lg"></i>
+          <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
           <div id=${id} class="quantity">0</div>
-          <i class="bi bi-plus-lg"></i>
+          <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
         </div>
       </div>
     </div>
@@ -57,3 +59,23 @@ let generateShop = () => {
 };
 
 generateShop();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+};
+
+let decrement = (id) => {
+  let selectedItem = id;
+};
+
+let update = () => {};
