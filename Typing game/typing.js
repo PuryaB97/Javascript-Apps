74e5ce8a41d2ea1c896fba421cb4f7f775979017
Purya Behdarvandan <purya.B97@gmail.com>
2022-09-18 23:59:35 +0300

    insert random words

--- Typing game/typing.js
+++ Typing game/typing.js
@@ -1,1 +1,25 @@
+const words =
+  "id one good real one not school set they state against public long number word about after much need open change also".split(
+    " "
+  );
+const wordsCount = words.length;
 
+function randomWord() {
+  const randomIndex = Math.ceil(Math.random() * wordsCount);
+  return words[randomIndex];
+}
+
+function formatWord(word) {
+  return `<div class="word"><span class="letter">${word
+    .split("")
+    .join("</span><span class='letter'>")}</span></div> `;
+}
+
+function newGame() {
+  document.getElementById("words").innerHTML = "";
+  for (let i = 0; i < 200; i++) {
+    document.getElementById("words").innerHTML += formatWord(randomWord());
+  }
+}
+
+newGame();
