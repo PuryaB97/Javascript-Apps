f89bc00df81fabf16c1bcf35252ceb5934891b33
Purya Behdarvandan <purya.B97@gmail.com>
2022-09-21 23:43:09 +0300

    handling space key

--- Typing game/typing.js
+++ Typing game/typing.js
@@ -34,9 +34,11 @@ function newGame() {
 
 document.getElementById("game").addEventListener("keyup", (e) => {
   const key = e.key;
+  const currentWord = document.querySelector(".word.current");
   const currentLetter = document.querySelector(".letter.current");
-  const expected = currentLetter.innerHTML;
+  const expected = currentLetter?.innerHTML || " ";
   const isLetter = key.length === 1 && key !== " ";
+  const isSpace = key === " ";
 
   console.log({ key, expected });
 
@@ -44,8 +46,27 @@ document.getElementById("game").addEventListener("keyup", (e) => {
     if (currentLetter) {
       addClass(currentLetter, key === expected ? "correct" : "incorrect");
       removeClass(currentLetter, "current");
-      addClass(currentLetter.nextSibling, "current");
+      if (currentLetter.nextSibling) {
+        addClass(currentLetter.nextSibling, "current");
+      }
     }
+  }
+
+  if (isSpace) {
+    if (expected !== " ") {
+      const lettersToInvalidate = [
+        ...document.querySelectorAll(".word.current .letter:not(.correct)"),
+      ];
+      lettersToInvalidate.forEach((letter) => {
+        addClass(letter, "incorrect");
+      });
+    }
+    removeClass(currentWord, "current");
+    addClass(currentWord.nextSibling, "current");
+    if (currentLetter) {
+      removeClass(currentLetter, "current");
+    }
+    addClass(currentWord.nextSibling.firstChild, "current");
   }
 });
 
