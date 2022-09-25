50b6dd628f78b8f4c7a48ce3a5f9216b498f65c3
Purya Behdarvandan <purya.B97@gmail.com>
2022-09-26 00:03:00 +0300

    Card flip

--- Memory card game/script.js
+++ Memory card game/script.js
@@ -1,1 +1,7 @@
-console.log("hello?!");
+const cards = document.querySelectorAll(".memory-card");
+
+function flipCard() {
+  this.classList.toggle("flip");
+}
+
+cards.forEach((card) => card.addEventListener("click", flipCard));
