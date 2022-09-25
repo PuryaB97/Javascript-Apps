6f6b5eeb0002320991dc854bf353f4f6b2cf526d
Purya Behdarvandan <purya.B97@gmail.com>
2022-09-26 00:16:30 +0300

    Store Cards

--- Memory card game/script.js
+++ Memory card game/script.js
@@ -1,7 +1,24 @@
 const cards = document.querySelectorAll(".memory-card");
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
   this.classList.toggle("flip");
+
+  if (!hasFlippedCard) {
+    // first click
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+
+    // do cards match
+
+    firstCard.dataset.framework;
+    secondCard.dataset.framework;
+  }
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
