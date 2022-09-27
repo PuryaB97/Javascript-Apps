c9cfe46e17b716982a8d4646db9b02f38f52e3a6
Purya Behdarvandan <purya.B97@gmail.com>
2022-09-27 21:14:09 +0300

    Card double click

--- Memory card game/script.js
+++ Memory card game/script.js
@@ -6,41 +6,48 @@ let firstCard, secondCard;
 
 function flipCard() {
   if (lockBoard) return;
-  this.classList.toggle("flip");
+  if (this === firstCard) return;
+
+  this.classList.add("flip");
 
   if (!hasFlippedCard) {
-    // first click
     hasFlippedCard = true;
     firstCard = this;
 
     return;
   }
 
-  hasFlippedCard = false;
   secondCard = this;
-
   checkForMatch();
 }
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-  isMatch ? disableCard() : unflibleCards();
+  isMatch ? disableCards() : unflipCards();
 }
 
-function disableCard() {
+function disableCards() {
   firstCard.removeEventListener("click", flipCard);
   secondCard.removeEventListener("click", flipCard);
+
+  resetBoard();
 }
 
-function unflibleCards() {
+function unflipCards() {
   lockBoard = true;
+
   setTimeout(() => {
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
 
-    lockBoard = false;
-  }, 1000);
+    resetBoard();
+  }, 1500);
+}
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
