2348548bfa9378e4d9356fcacbaa184687a9f1cb
Purya Behdarvandan <purya.B97@gmail.com>
2022-09-28 23:56:01 +0300

    Working on mistakes

--- Typing game/js/script.js
+++ Typing game/js/script.js
@@ -1,7 +1,8 @@
 const typingText = document.querySelector(".typing-text p");
 inpField = document.querySelector(".wrapper .input-field");
+mistakeTag = document.querySelector(".mistake span");
 
-let charIndex = 0;
+let charIndex = (mistakes = 0);
 
 function randomPragraph() {
   let randIndex = Math.floor(Math.random() * paragraphs.length);
@@ -17,19 +18,25 @@ function randomPragraph() {
 function initTyping() {
   const characters = typingText.querySelectorAll("span");
   let typedChar = inpField.value.split("")[charIndex];
-  if ((typedChar = null)) {
+  if (typedChar == null) {
     charIndex--;
+    if (characters[charIndex].classList.contains("incorrect")) {
+      mistakes--;
+    }
     characters[charIndex].classList.remove("correct", "incorrect");
   } else {
     if (characters[charIndex].innerText === typedChar) {
       characters[charIndex].classList.add("correct");
     } else {
+      mistakes++;
       characters[charIndex].classList.add("incorrect");
     }
     charIndex++;
   }
   characters.forEach((span) => span.classList.remove("active"));
   characters[charIndex].classList.add("active");
+
+  mistakeTag.innerHTML = mistakes;
 }
 
 randomPragraph();
